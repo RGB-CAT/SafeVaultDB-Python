093e34325a2356248db8a3706630581fcea8c35a
RGB-CAT <72267692+RGB-CAT@users.noreply.github.com>
2024-03-13 21:47:02 -0400

    Update database.py

--- SafeVaultDB/SafeVaultDB/database.py
+++ SafeVaultDB/SafeVaultDB/database.py
@@ -6,17 +6,48 @@ import json
 
 class SafeVault:
     def __init__(self, database_path, password):
+        """
+        Function Usage: __init__(database_path:str, password:str)
+        
+        Function Description:
+            Initializes the SafeVault object with the given database path and password.
+        
+        Args:
+            database_path (str): The path to the database file.
+            password (str): The password used to generate the encryption key.
+        """
         self.database_path = database_path
         self.key = self.generate_key_from_password(password)
         self.valid_key = self.check_key()
         self.create_database_file()
 
     def generate_key_from_password(self, password):
+        """
+        Function Usage: generate_key_from_password(password:str)
+        
+        Function Description:
+            Generates an encryption key using the provided password.
+        
+        Args:
+            password (str): The password used to generate the encryption key.
+        
+        Returns:
+            bytes: The generated encryption key.
+        """
         hashed_text = hashlib.sha256(password.encode()).digest()
         key = hashed_text[:32]
         return base64.urlsafe_b64encode(key)
 
     def check_key(self):
+        """
+        Function Usage: check_key()
+        
+        Function Description:
+            Checks if the encryption key is valid by attempting to decrypt data.
+        
+        Returns:
+            bool: True if the key is valid, False otherwise.
+        """
         if not os.path.exists(self.database_path):
                     # Create the database file if it doesn't exist
                     self.create_database_file()
@@ -31,6 +62,12 @@ class SafeVault:
             return False
 
     def create_database_file(self):
+        """
+        Function Usage: create_database_file()
+        
+        Function Description:
+            Creates the database file if it doesn't exist and encrypts an empty dictionary.
+        """
         if not os.path.exists(self.database_path):
             try:
                 # Encrypt a placeholder string
@@ -43,6 +80,15 @@ class SafeVault:
                 print("An error occurred while creating the database file:", e)
 
     def _read_database(self):
+        """
+        Function Usage: _read_database()
+        
+        Function Description:
+            Reads data from the encrypted database file.
+        
+        Returns:
+            str: Decrypted data from the database.
+        """
         if not os.path.exists(self.database_path):
             # Create the database file if it doesn't exist
             self.create_database_file()
@@ -59,6 +105,15 @@ class SafeVault:
                 return ''
 
     def _write_database(self, data):
+        """
+        Function Usage: _write_database(data:str)
+        
+        Function Description:
+            Writes data to the encrypted database file.
+        
+        Args:
+            data (str): Data to be written to the database.
+        """
         try:
             cipher = Fernet(self.key)
             encrypted_data = cipher.encrypt(data.encode())
@@ -69,6 +124,16 @@ class SafeVault:
 
 
     def add_data(self, key, value):
+        """
+        Function Usage: add_data(key:str, value:str)
+        
+        Function Description:
+            Adds key-value pair to the encrypted database.
+        
+        Args:
+            key (str): The key to add.
+            value (str): The value corresponding to the key.
+        """
         if self.valid_key:
             data = self._read_database()
             data_dict = {} if not data else json.loads(data)
@@ -76,6 +141,19 @@ class SafeVault:
             self._write_database(json.dumps(data_dict))
 
     def update_data(self, key, value):
+        """
+        Function Usage: update_data(key:str, value:str)
+        
+        Function Description:
+            Updates the value corresponding to the given key in the encrypted database.
+        
+        Args:
+            key (str): The key to update.
+            value (str): The new value corresponding to the key.
+        
+        Returns:
+            bool: True if the update was successful, False otherwise.
+        """
         if self.valid_key:
             data = self._read_database()
             data_dict = {} if not data else json.loads(data)
@@ -86,6 +164,18 @@ class SafeVault:
         return False
 
     def get_data(self, key):
+        """
+        Function Usage: get_data(key:str)
+        
+        Function Description:
+            Retrieves data corresponding to the given key from the encrypted database.
+        
+        Args:
+            key (str): The key to retrieve data for.
+        
+        Returns:
+            str: The value corresponding to the key, or None if key not found.
+        """
         if self.valid_key:
             data = self._read_database()
             if data:
@@ -94,6 +184,18 @@ class SafeVault:
         return None
 
     def delete_data(self, key):
+        """
+        Function Usage: delete_data(key:str)
+        
+        Function Description:
+            Deletes data corresponding to the given key from the encrypted database.
+        
+        Args:
+            key (str): The key to delete data for.
+        
+        Returns:
+            bool: True if the data was successfully deleted, False otherwise.
+        """
         if self.valid_key:
             data = self._read_database()
             if data:
